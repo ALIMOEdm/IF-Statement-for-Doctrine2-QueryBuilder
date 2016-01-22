c25d85748da36ed715249d0e64a0b5d5b1be152e
Michael <neradivx@lenta.ru>
2016-01-22 16:49:22 +0500

    Create IfExpression.php

---
+++ IfExpression.php
@@ -0,0 +1,202 @@
+<?php
+namespace AppBundle\DQL;
+
+use Doctrine\ORM\Query\AST\InputParameter;
+use Doctrine\ORM\Query\AST\LikeExpression;
+use Doctrine\ORM\Query\AST\NullComparisonExpression;
+use Doctrine\ORM\Query\AST\QuantifiedExpression;
+use Doctrine\ORM\Query\Lexer;
+
+class IfExpression extends \Doctrine\ORM\Query\AST\Functions\FunctionNode{
+
+    public $firstDateExpression = null;
+    public $secondDateExpression = null;
+    public $thirdDateExpression = null;
+    public $unit = null;
+    public $result = array();
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser, $isOrAnd=false)
+    {
+        if($parser->getLexer()->isNextToken(Lexer::T_IDENTIFIER) && strtoupper($parser->getLexer()->lookahead['value']) == 'IF'){
+            $parser->match(Lexer::T_IDENTIFIER);
+            $this->result[] = ' IF ';
+            $parser->match(Lexer::T_OPEN_PARENTHESIS);
+            $this->result[] = ' ( ';
+        }
+        $stringExpr = $parser->StringExpression();
+        $not = false;
+
+         if ($parser->getLexer()->isNextToken(Lexer::T_NOT)) {
+             $parser->match(Lexer::T_NOT);
+             $not = true;
+         }
+
+        //If next like
+        if($parser->getLexer()->isNextToken(Lexer::T_LIKE)) {
+            $parser->match(Lexer::T_LIKE);
+            if ($parser->getLexer()->isNextToken(Lexer::T_INPUT_PARAMETER)) {
+                $parser->match(Lexer::T_INPUT_PARAMETER);
+                $stringPattern = new InputParameter($parser->getLexer()->token['value']);
+            } else {
+                $stringPattern = $parser->StringPrimary();
+            }
+
+            $escapeChar = null;
+
+            if ($parser->getLexer()->lookahead['type'] === Lexer::T_ESCAPE) {
+                $parser->match(Lexer::T_ESCAPE);
+                $parser->match(Lexer::T_STRING);
+                $escapeChar = $parser->getLexer()->token['value'];
+            }
+
+            $likeExpr = new LikeExpression($stringExpr, $stringPattern, $escapeChar);
+            $likeExpr->not = $not;
+
+            $this->result[] = $likeExpr;
+        }else
+        //IS NULL
+        if($parser->getLexer()->isNextToken(Lexer::T_IS)){
+            $nullCompExpr = new NullComparisonExpression($stringExpr);
+            $parser->match(Lexer::T_IS);
+
+            if ($parser->getLexer()->isNextToken(Lexer::T_NOT)) {
+                $parser->match(Lexer::T_NOT);
+                $nullCompExpr->not = true;
+            }
+            $parser->match(Lexer::T_NULL);
+
+            $this->result[] = $nullCompExpr;
+        }
+        else{
+            $comp = $this->compationOper($parser);
+            if($comp){
+                $this->result[] = $stringExpr;
+                $this->result[] = $comp;
+                $this->result[] = $parser->ArithmeticPrimary();
+            }
+        }
+
+        //OR
+        if($parser->getLexer()->isNextToken(Lexer::T_OR)){
+            $parser->match(Lexer::T_OR);
+            $this->result[] = ' OR ';
+
+            $this->parse($parser, true);
+        }
+
+        if($isOrAnd){
+            return;
+        }
+
+        $parser->match(Lexer::T_COMMA);
+        $this->result[] = ', ';
+
+        if($parser->getLexer()->isNextToken(Lexer::T_IDENTIFIER) && strtoupper($parser->getLexer()->lookahead['value']) == 'IF'){
+            $this->parse($parser);
+        }
+        else{
+            $this->result[] = $parser->ArithmeticPrimary();
+
+            $comp = $this->compationOper($parser);
+            if($comp){
+                $this->result[] = $comp;
+                $this->result[] = $parser->ArithmeticPrimary();
+            }
+        }
+        $parser->match(Lexer::T_COMMA);
+        $this->result[] = ', ';
+
+        if($parser->getLexer()->isNextToken(Lexer::T_IDENTIFIER) && strtoupper($parser->getLexer()->lookahead['value']) == 'IF'){
+            $this->parse($parser);
+        } else{
+
+            $this->result[] = $parser->ArithmeticPrimary();
+            $comp = $this->compationOper($parser);
+            if($comp){
+                $this->result[] = $comp;
+                $this->result[] = $parser->ArithmeticPrimary();
+            }
+
+
+        }
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $this->result[] = ' ) ';
+    }
+
+
+    public function compationOper($parser){
+        switch ($parser->getLexer()->lookahead['value']) {
+            case '=':
+                $parser->match(Lexer::T_EQUALS);
+
+                return '=';
+
+            case '<':
+                $parser->match(Lexer::T_LOWER_THAN);
+                $operator = '<';
+
+                if ($parser->getLexer()->isNextToken(Lexer::T_EQUALS)) {
+                    $parser->match(Lexer::T_EQUALS);
+                    $operator .= '=';
+                } else if ($parser->getLexer()->isNextToken(Lexer::T_GREATER_THAN)) {
+                    $parser->match(Lexer::T_GREATER_THAN);
+                    $operator .= '>';
+                }
+
+                return $operator;
+
+            case '>':
+                $parser->match(Lexer::T_GREATER_THAN);
+                $operator = '>';
+
+                if ($parser->getLexer()->isNextToken(Lexer::T_EQUALS)) {
+                    $parser->match(Lexer::T_EQUALS);
+                    $operator .= '=';
+                }
+
+                return $operator;
+
+            case '!':
+                $parser->match(Lexer::T_NEGATE);
+                $parser->match(Lexer::T_EQUALS);
+
+                return '<>';
+
+            default:
+                return false;
+        }
+    }
+
+
+    public function printT(){
+        $arr = $this->result;
+        while(true){
+            $per =  array_shift($arr);
+            if(is_null($per)){
+                break;
+            }
+            if(is_object($per)){
+                var_dump($per);
+            }else{
+                var_dump($per);
+            }
+        }
+
+    }
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        $str = '';
+        while(true){
+            $per =  array_shift($this->result);
+            if(is_null($per)){
+                break;
+            }
+            if(is_object($per)){
+                $str .= ' '.$per->dispatch($sqlWalker).' ';
+            }else{
+                $str .= $per;
+            }
+        }
+        return $str;
+    }
+}
